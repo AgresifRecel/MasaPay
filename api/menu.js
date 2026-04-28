7e4e6110f8df8feee87c9f8cc3bb21c18930be60
AgresifRecel <bugratitulgaqnk@gmail.com>
2026-04-28 23:15:32 +0300

    Create menu.js

---
+++ api/menu.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://europe-west3-paragastroteka-inventory.cloudfunctions.net/getQrMenu?customerId=sakal-kafe-bar&branchId=sakal-kafe-bar-1"
+    );
+
+    const data = await response.json();
+
+    // 🔥 Menü parse (temiz format)
+    const all = [];
+
+    data.categories.forEach((cat) => {
+      (cat.sections || []).forEach((sec) => {
+        (sec.products || []).forEach((p) => {
+          all.push({
+            name: p.name,
+            price: p.price,
+            category: cat.name
+          });
+        });
+      });
+    });
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(all);
+  } catch (err) {
+    res.status(500).json({ error: "menu alınamadı" });
+  }
+}
